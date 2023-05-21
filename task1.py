9b086d78698e6ef309dc9474bd20a1af4d7835ae
g-saradha <134219307+g-saradha@users.noreply.github.com>
2023-05-21 22:02:51 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,2 +1,14 @@
-c=a+b
-print c
+nterms = int(input("Number of terms"))
+n1, n2 = 0, 1
+count = 0
+if nterms == 1:
+   print("Fibonacci sequence:")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
